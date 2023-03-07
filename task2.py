30d43b414d51073f3b7ae2b39e72ea778b38b90a
probablyalright <112282114+probablyalright@users.noreply.github.com>
2023-03-07 21:39:04 +0200

    Task 2
    Practical work 4, Task #2
    For Python university course.

---
+++ task2.py
@@ -0,0 +1,36 @@
+import os
+# insert working directory here:
+os.chdir("C:\\Users\\...")
+
+fhand = open('mbox-short.txt').readlines()
+
+def get_email_from_str(str):
+    return str.split(' ')[1].strip()
+
+def get_domain_from_email(str):
+    return str.split('@')[1].strip()
+
+def count_domains(file = fhand):
+    count_dict = dict()
+    for line in file:
+        if line.startswith('From'):
+            email = get_email_from_str(line)
+            domain = get_domain_from_email(email)
+            if domain not in count_dict:
+                count_dict[domain] = 1
+            else:
+                count_dict[domain]+= 1
+    return count_dict
+
+def display_histogram(cdict):
+    for k, v in sorted(cdict.items()):
+        print(k + ": " + str(v), end=' ')
+        i = 0
+        while i <= v:
+            print('*', end='')
+            i+=1
+        print()
+
+print('SORTED:') 
+cdict = count_domains(fhand)
+display_histogram(cdict)
